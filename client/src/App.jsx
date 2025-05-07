f9f2640085bf550dfd5fd1a5e96efe609d034f0a
Amin sheikh <aminsheikh9065@gmail.com>
2025-05-07 07:20:14 +0100

    Update App.jsx
    heading changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,7 @@ const App = () => {
 
   return (
     <div className=' bg-black text-2xl min-h-screen flex items-center justify-center text-white '>
-<<<<<<< HEAD
       <h1 className=' uppercase'>this is creatorsLand web</h1>
-=======
-      <h1 className=' uppercase'>this is influencer webbb</h1>
->>>>>>> cacdcd627b682486b46ccbca50fd5e0d78b7ceaa
     </div>
   )
 }
